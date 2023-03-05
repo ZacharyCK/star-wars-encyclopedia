727058d519a14d594762a1512fda9597bccdb812
Zachary Kohs <zachary.c.kohs@gmail.com>
2023-03-05 16:53:40 -0500

    Created star wars encyclopedia app and made a title bar component with the title

---
+++ src/Titlebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Titlebar.css";
+
+function Titlebar() {
+  return (
+    <div className="title-bar">
+      <h1 id="app-title">Star Wars Encyclopedia</h1>
+    </div>
+  );
+}
+
+export default Titlebar;
